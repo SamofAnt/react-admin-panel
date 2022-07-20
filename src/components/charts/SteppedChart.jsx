698f6f7cea5feb4e58a8f0bba9def49357e56f15
samof <samofalov.19982805@gmail.com>
2022-07-20 11:53:14 +0300

    added more bars in charts

--- src/components/charts/SteppedChart.jsx
+++ src/components/charts/SteppedChart.jsx
@@ -50,8 +50,9 @@ export const SteppedChart = ({ data}) =>{
           height={400 }
           data={data}
           margin={{
-            top: 5,
-            left: 10,
+            top: 20,
+            right: 30,
+            left: 20,
             bottom: 5,
           }}
         >
@@ -60,7 +61,13 @@ export const SteppedChart = ({ data}) =>{
           <YAxis />
           <Tooltip />
           <Legend />
-          <Bar dataKey="countOfStatusesA" fill="#8884d8" />
+          <Bar dataKey="countOfStatusesA" fill="#00FA9A" />
+          <Bar dataKey="countOfStatusesC" fill="#F4A460" />
+          <Bar dataKey="countOfStatusesD" fill="#7CFC00" />
+          <Bar dataKey="countOfStatusesL" fill="#7B68EE" />
+          <Bar dataKey="countOfStatusesN" fill="#FFFF00" />
+          <Bar dataKey="countOfStatusesP" fill="#8884d8" />
+          <Bar dataKey="countOfStatusesE" fill="#FF0000" />
         </BarChart>
         </div>
     );
